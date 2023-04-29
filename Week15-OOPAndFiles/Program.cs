1e2c56615330b47b72f254cb373bec217b959b94
Martin Pruuliste <mpruulis@tktk.ee>
2023-04-29 12:56:52 +0300

    add week15 code

---
+++ Week15-OOPAndFiles/Program.cs
@@ -0,0 +1,59 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2], tempArray[1]);
+    myMovies.Add(newMovie);
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was released in {movie.Year}. Rating: {movie.Rating}");
+}
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\Users\Lenovo\source\repos\Week15-OOPAndFiles\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+
+class Movie
+{
+    string title; //fields
+    string year;
+    string rating;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+    public string Rating
+    {
+        get { return rating; }
+    }
+    public Movie(string _title, string _year, string _rating)
+    {
+        title = _title;
+        year = _year;
+        rating = _rating;
+    }
+}
